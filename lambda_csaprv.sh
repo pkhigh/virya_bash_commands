3efc4d3625c1de692c0977cc6f453f4a8ce031f3
Paras Kapoor <paras@roadzen.io>
2021-08-28 19:39:06 -0400

    scripts

--- lambda_csaprv.sh
+++ lambda_csaprv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#$ -N bmvc_afhq_128_lambda_ciadv
+#$ -N bmvc_afhq_128_lambda_csaprv
 #$ -j y
 #$ -l h=virya
 #$ -l h_vmem=250G
@@ -7,11 +7,11 @@
 module load anaconda/3.2019.10/default
 module load cuda/10.2/default
 source activate stargan-v2
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4
 cd paper_work/tini_starganv2/
 
 python main.py --mode distill_train --num_domains 3 --w_hpf 0 --batch_size 8 --num_workers 4 --resume_iter 0  --alpha 128 --efficient 1 \
-               --lambda_reg 1 --lambda_sty 1 --lambda_ds 2 --lambda_cyc 1 --f_lr 1e-4 --lambda_ciadv 1 --lambda_csadv 0 --lambda_csaprv 0 --lambda_csut 0 \
+               --lambda_reg 1 --lambda_sty 1 --lambda_ds 2 --lambda_cyc 1 --f_lr 1e-4 --lambda_ciadv 1 --lambda_csadv 1 --lambda_csaprv 1 --lambda_csut 1 \
                --train_img_dir data/afhq/train --val_img_dir data/afhq/val \
                --teacher_checkpoint_dir expr/checkpoints/afhq --teacher_resume_iter 100000 \
-               --checkpoint_dir expr/bmvc_afhq_128_lambda_ciadv --print_every 10 --save_every 10000
+               --checkpoint_dir expr/bmvc_afhq_128_lambda_csaprv --print_every 10 --save_every 10000
