c6f9c2a04750c1a535297cc24d224c0275b36ac0
Paras Kapoor <paras@roadzen.io>
2021-09-01 12:39:31 -0400

    evaluation script 1

---
+++ eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#$ -N bmvc_afhq_128_test
+#$ -j y
+#$ -l h=virya
+#$ -l g=1
+#$ -l h_vmem=300G
+# set environment
+module load anaconda/3.2019.10/default
+module load cuda/10.2/default
+source activate stargan-v2
+export CUDA_VISIBLE_DEVICES=0
+cd paper_work/tini_starganv2/
+
+python main.py --mode eval --num_domains 3 --resume_iter 100000 --w_hpf 0 --alpha 128 --efficient 1 \
+               --train_img_dir data/afhq/train --val_img_dir data/afhq/val \
+               --checkpoint_dir expr/bmvc_afhq_128 --eval_dir expr/bmvc_afhq_128
