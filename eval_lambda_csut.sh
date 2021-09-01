d0aab965ac4706abde8ec615fbd4ebab7c240846
Paras Kapoor <paras@roadzen.io>
2021-09-01 17:36:34 -0400

    updated eval commands

--- eval_lambda_csut.sh
+++ eval_lambda_csut.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#$ -N bmvc_afhq_128_test
+#$ -N bmvc_afhq_128_lambda_csut2_test
 #$ -j y
 #$ -l h=virya
 #$ -l g=1
@@ -13,4 +13,4 @@ cd paper_work/tini_starganv2/
 
 python main.py --mode eval --num_domains 3 --resume_iter 100000 --w_hpf 0 --alpha 128 --efficient 1 \
                --train_img_dir data/afhq/train --val_img_dir data/afhq/val \
-               --checkpoint_dir expr/bmvc_afhq_128 --eval_dir expr/bmvc_afhq_128
+               --checkpoint_dir expr/bmvc_afhq_128_lambda_csut2 --eval_dir expr/eval/bmvc_afhq_128_lambda_csut2
