6783fd571a79deda3a2fec93591cf0f0256722d2
Paras Kapoor <paras@roadzen.io>
2021-09-02 21:58:14 -0400

    pre train test

--- bmvc_test.sh
+++ bmvc_test.sh
@@ -11,38 +11,10 @@ source activate stargan-v2
 export CUDA_VISIBLE_DEVICES=0
 cd paper_work/tini_starganv2/
 
-conda list
-
-python main.py --mode sample --num_domains 2 --w_hpf 1 \
+python main.py --mode eval --num_domains 3 --w_hpf 0 \
                --resume_iter 100000 \
-               --checkpoint_dir expr/checkpoints/celeba_hq \
-               --val_batch_size 4 --latent_sample_per_domain 1 \
-               --filename celeba_hq.jpg \
-               --src_dir assets/paper/celeba_hq/src \
-               --ref_dir assets/paper/celeba_hq/ref
-               
-python main.py --mode sample --num_domains 3 --w_hpf 0 \
-               --resume_iter 100000 \
-               --checkpoint_dir expr/checkpoints/afhq \
-               --val_batch_size 4 --latent_sample_per_domain 1 \
-               --filename afhq.jpg \
-               --src_dir assets/paper/afhq/src \
-               --ref_dir assets/paper/afhq/ref
-               
-python main.py --mode sample --num_domains 2 --w_hpf 1 \
                --alpha 128 --efficient 1 \
-               --resume_iter 100000 \
-               --checkpoint_dir expr/checkpoints/tiny_celeba_hq \
-               --val_batch_size 4 --latent_sample_per_domain 1 \
-               --filename tiny_celeba_hq.jpg \
-               --src_dir assets/paper/celeba_hq/src \
-               --ref_dir assets/paper/celeba_hq/ref
-
-python main.py --mode sample --num_domains 3 --w_hpf 0 \
-               --alpha 128 --efficient 1 \
-               --resume_iter 100000 \
+               --train_img_dir data/afhq/train \
+               --val_img_dir data/afhq/val \
                --checkpoint_dir expr/checkpoints/tiny_afhq \
-               --val_batch_size 4 --latent_sample_per_domain 1 \
-               --filename tiny_afhq.jpg \
-               --src_dir assets/paper/afhq/src \
-               --ref_dir assets/paper/afhq/ref
+               --eval_dir expr/eval/tiny_afhq
